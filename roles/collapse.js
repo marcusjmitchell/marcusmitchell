6357430c2e1fa92768fd3522480e7ea9b05c93f7
marcusjmitchell <jayzandkanyeinparis@protonmail.com>
2023-10-04 19:06:47 +0100

    try again...

--- roles/collapse.js
+++ roles/collapse.js
@@ -9,23 +9,8 @@ for (i = 0; i < coll.length; i++) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
+      var parent = this.parentElement;
+      parent.style.maxHeight = parent.scrollHeight + "px";
     }
-  });
-}
-
-var colll = document.getElementsByClassName("subcollapsible");
-var i;
-
-for (i = 0; i < colll.length; i++) {
-  colll[i].addEventListener("click", function() {
-    this.classList.toggle("revealed");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-    var parent = this.parentElement;
-    parent.style.maxHeight = parent.scrollHeight + "px";
   });
 }
