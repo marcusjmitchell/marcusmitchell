77c7e73815c0c7a21ade0ef176471a2baf3df146
marcusjmitchell <jayzandkanyeinparis@protonmail.com>
2023-10-04 19:04:21 +0100

    fix again?

--- roles/collapse.js
+++ roles/collapse.js
@@ -12,3 +12,20 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+var colll = document.getElementsByClassName("subcollapsible");
+var i;
+
+for (i = 0; i < colll.length; i++) {
+  colll[i].addEventListener("click", function() {
+    this.classList.toggle("revealed");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+    var parent = this.parentElement;
+    parent.style.maxHeight = parent.scrollHeight + "px";
+  });
+}
